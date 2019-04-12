da0adf0e6b8620a64abe496ecb668c44a45d28d5
sidghome16@gmail.com <siddy1234>
2019-04-12 19:46:10 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -12,7 +12,10 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.',
+  'example4':
+      'One of the most overrated places on Earth!'
+      
 };
 
 function status(statusText) {
